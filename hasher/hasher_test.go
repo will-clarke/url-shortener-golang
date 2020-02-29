d2c65be2c4f73a535becc2806538852e371f614e
William Clarke <wmmclarke@gmail.com>
2020-02-29 16:10:16 +0000

    improve hasher_test ; make it more explicit & test a bit more

--- hasher/hasher_test.go
+++ hasher/hasher_test.go
@@ -1,15 +1,34 @@
 package hasher_test
 
 import (
+	"strings"
 	"testing"
 	"url-shortener/hasher"
 	"url-shortener/shortener"
 )
 
+const validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-"
+
 func TestHash(t *testing.T) {
-	url := shortener.URL("https://example.com/foo")
-	code := hasher.Hash(url)
-	if len(code) != hasher.SIZE {
-		t.Error("hahsed", url, "and got unexpected code", code)
+	urls := []shortener.URL{
+		"",
+		"https://www.example.com/legit-url",
+		"https://www.example.com/another-url",
+		"any other string",
+	}
+	for _, url := range urls {
+		t.Run("TestHash-"+string(url), func(t *testing.T) {
+			code := hasher.Hash(url)
+			if len(code) != hasher.SIZE {
+				t.Error("hahsed", url, "and got unexpected code", code)
+			}
+			for _, c := range code {
+				if !strings.Contains(validChars, string(c)) {
+					t.Error("was expecting", string(c), "in", code,
+						"to only contain these characters:", validChars)
+				}
+
+			}
+		})
 	}
 }
